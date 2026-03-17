ad5647a7bdeac322a702a80034406215adafcdc8
NilssonAvH <nlr519.com@gmail.com>
2026-03-16 21:27:43 -0500

    [+] Se arregla apiUrl de acuerdo al backend en EC2 de AWS

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true, // ← true en producción
-    apiUrl: 'https://api.tupagina.com/api', // URL de producción de backend
+    apiUrl: 'http://ec2-52-15-36-165.us-east-2.compute.amazonaws.com:3000/api', // URL de producción de backend
     usersSlug: '/v1/users',
     clientsSlug: '/v1/clients',
     appName: 'Delfos CRN'
